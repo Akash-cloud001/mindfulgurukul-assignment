303251113737681687eea91c49b6106e73a5e072
Akash Parmar <ak.parmar1442000@gmail.com>
2024-01-02 02:55:04 +0530

    config changed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     './src/**/*.{js,ts,jsx,tsx}', 
-    './public/index.html',
+    './index.html',
     '.src/components/**/*.{js,ts,jsx,tsx}',
     '.src/pages/**/*.{js,ts,jsx,tsx}',
   ],
