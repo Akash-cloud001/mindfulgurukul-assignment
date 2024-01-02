8d4b8beade93ab93a083eaa9eb5ce3d1f6de8bb4
Akash Parmar <ak.parmar1442000@gmail.com>
2024-01-02 12:16:22 +0530

    tailwind.config changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // content: [
-  //   './**/*.{html,js,jsx}'
-  //   // './src/**/*.{js,ts,jsx,tsx}', 
-  //   // './index.html',
-  //   // './src/components/**/*.{js,ts,jsx,tsx}',
-  //   // './src/pages/**/*.{js,ts,jsx,tsx}',
-  // ],
-  content: ["./dist/**/*.{html,css ,js,jsx}"],
+  content: [
+    './src/components/**/*.{html,js,jsx}',
+    './src/pages/**/*.{html,js,jsx}',
+    './index.html',
+  ],
   theme: {
     extend: {
       height:{
